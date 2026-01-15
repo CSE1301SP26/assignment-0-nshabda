d77ce97211cf9beaf3f531691e4cfa2dae05d2b5
nshabda <n.shabda@wustl.edue>
2026-01-15 15:56:20 -0600

    Assigment 0

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,24 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
-		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(70);
+		robot.forward(70);
+		robot.turnRight(50); 
+		robot.forward(140);
+		robot.turnRight(50);
+		robot.forward(140);
+		robot.turnRight(50); 
+		robot.forward(140);
+		robot.turnRight(50);
+		robot.forward(140);
+		robot.turnRight(50);
+		robot.forward(140);
+		robot.turnRight(50);
+		robot.forward(140);
+		robot.turnRight(50);
+		robot.forward(40);
+
+		
 	}
 
 }
